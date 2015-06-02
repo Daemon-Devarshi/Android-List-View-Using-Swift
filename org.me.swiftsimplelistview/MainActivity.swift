bc56891a78ecf5d223e05b6bda5e5706ecc2a4f6
devarshi <kulshreshtha.devarshi@gmail.com>
2015-06-03 01:21:38 +0530

    up..up.. and away..

---
+++ org.me.swiftsimplelistview/MainActivity.swift
@@ -0,0 +1,31 @@
+﻿import java.util
+import  android.app
+import  android.content
+import  android.os
+import  android.util
+import  android.view
+import  android.widget
+
+public class MainActivity: Activity {
+	var listView : ListView?
+	public override func onCreate(savedInstanceState: Bundle!) {
+		super.onCreate(savedInstanceState)
+		ContentView = R.layout.main
+		
+		// get listView object
+		listView = findViewById(R.id.list) as! ListView
+		
+		// array initialization
+		let values = ["Devarshi", "Kulshreshtha", "is", "Genius"]
+		
+		// defining a new adapter
+		// 1st parameter: context
+		// 2nd: layout for the row
+		// 3rd: ID of textview to which data is written
+		// 4th: array of data
+		let adapter = ArrayAdapter(self, android.R.layout.simple_list_item_1, android.R.id.text1, values)
+		
+		// Assigning adapter to list view
+		listView!.setAdapter(adapter)
+	}
+}
